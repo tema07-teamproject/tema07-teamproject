5c6e5b89447101d7785f86823f3076e2cf9dbc71
Emsk0004 <emsk0004@stud.kea.dk>
2025-02-17 15:46:54 +0100

    feature_produktliste_done

--- produktliste/produktliste.js
+++ produktliste/produktliste.js
@@ -1,41 +1,43 @@
 const mycategory = new URLSearchParams(window.location.search).get("category");
 let listContainer = document.querySelector(".produkt");
 
-const overskrift = document.querySelector("h1");
-overskrift.innerHTML = mycategory;
+if (mycategory) {
+  document.querySelector("h1").innerText = mycategory;
+}
 
-// Henter data fra dummyjson API'et
 fetch("https://dummyjson.com/products")
   .then((response) => response.json())
-  .then((data) => showList(data.products)); // API'en returnerer et objekt med en "products" liste
+  .then((data) => showList(data.products));
 
 function showList(products) {
-  console.log(products);
-  const markup = products
-    .map(
-      (product) => `
-            <div class="item">
-                <div class="image-wrapper">
-                    <img class="produkt_img" src="${product.thumbnail}" alt="${product.title}">
-                    
-                    <div class="discount ${product.discountPercentage ? "" : "hide"}">
-                        ${product.discountPercentage}%
-                    </div>
-                    <div class="low_stock ${product.stock < 10 ? "" : "hide"}">
-                        Low Stock (${product.stock} left)
-                    </div>
-                </div>
+  const filteredProducts = products.filter((product) => product.tags.includes(mycategory)); // Filtrér produkter
 
-                <h2>${product.title}</h2>
-                <div class="price">
-                    <p class="${product.discountPercentage ? "foer_pris" : ""}">${product.price} €</p>
-                    <p class="${product.discountPercentage ? "" : "hide"}">
-                        ${(product.price * (1 - product.discountPercentage / 100)).toFixed(2)} €
-                    </p>
+  const markup = filteredProducts
+    .map(
+      (product) =>
+        `<div class="item">
+            <div class="image-wrapper">
+                <img class="produkt_img" src="${product.thumbnail}" alt="${product.title}">
+                
+                <div class="discount ${product.discountPercentage ? "" : "hide"}">
+                    ${product.discountPercentage}%
                 </div>
-                <a href="produkt.html?id=${product.id}">Read more</a>
-            </div>`
+                <div class="low_stock ${product.stock < 10 ? "" : "hide"}">
+                    Low Stock (${product.stock} left)
+                </div>
+            </div>
+
+            <h2>${product.title}</h2>
+            <div class="price">
+                <p class="${product.discountPercentage ? "foer_pris" : ""}">${product.price} €</p>
+                <p class="${product.discountPercentage ? "" : "hide"}">
+                    ${(product.price * (1 - product.discountPercentage / 100)).toFixed(2)} €
+                </p>
+            </div>
+            <a href="/html/produkt.html?id=${product.id}">Read more</a>
+        </div>`
     )
     .join("");
+
   listContainer.innerHTML = markup;
 }
